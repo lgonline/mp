e5f614c9cee6593f902bc4d6066454dbad7e3803
lgonline <lg_online@hotmail.com>
2015-10-16 01:11:12 +0800

    lgonline 20151016

--- src/graph/moveturtle.py
+++ src/graph/moveturtle.py
@@ -6,4 +6,10 @@ import turtle
 
 
 if __name__ == "__main__":
-    pass
+    t = turtle.Pen()
+    for x in range(1,19):
+        t.forward(100)
+        if x % 2 == 0:
+            t.left(175)
+        else:
+            t.left(225)
