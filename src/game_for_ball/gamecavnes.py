7e160856fdfcf6f0784539231df53a283adbf08b
lgonline <lg_online@hotmail.com>
2015-10-20 23:45:44 +0800

    lgonline 20151019

---
+++ src/game_for_ball/gamecavnes.py
@@ -0,0 +1,13 @@
+#/usr/bin/python
+__author__ = 'hcm'
+
+from tkinter import *
+
+if __name__ == "__main__":
+    tk = Tk()
+
+    cavnes = Canvas(tk,width=500,height=500)
+
+    cavnes.pack()
+
+    cavnes.mainloop()
