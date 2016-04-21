034e55ce9edc7a4be5e73470feb087020c2d0bd0
lg_online <lg_online@hotmail.com>
2016-04-21 21:55:42 +0800

    20160421

---
+++ src/fundamental/core_knowledge/forDataType.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+author = 'ethan'
+
+int_type = 4
+string_type = "hello"
+tuple_type = (1,2,3,4,5,6,7,8)
+list_type = ['a','b','c','d']
+dict_type = {'key1':'aaa','key2':'bbb','key3':'ccc'}
+
+if __name__ == "__main__":
+    print('the type of int_type is ',type(int_type),', and the value is ',int_type)
+    print('the type of int_type is ', type(string_type), ', and the value is ', string_type)
+    print('the type of int_type is ', type(tuple_type), ', and the value is ', tuple_type)
+    print('the type of int_type is ', type(list_type), ', and the value is ', list_type)
+    print('the type of int_type is ', type(dict_type), ', and the value is ', dict_type)
