c41a57cc24dd389cf8fc0966cc348e063439eae0
lg_online <lg_online@hotmail.com>
2018-06-04 10:11:01 +0800

    update 20180601

--- src/practices/ipinfo/main.py
+++ src/practices/ipinfo/main.py
@@ -11,24 +11,18 @@
 https://github.com/17mon/python/blob/master/main.py
 """
 
-from imp import reload
-import sys,os
-reload(sys)
-from ..ipinfo.handle_ip import IP
-# from src.practices.ipinfo.ipinfo import IP
-# from ..ipinfo.ipinfo import IPX
+# from imp import reload
+import os
+# reload(sys)
+# from handle_ip import IP
+from src.practices.ipinfo.handle_ip import IP
+from src.practices.ipinfo.handle_ip import IPX
 
 # sys.setdefaultencoding('ut-8')
 
 IP.load(os.path.abspath('mydata4vipday2.dat'))
 print(IP.find('118.28.8.8'))
 
-# IPX.load(os.path.abspath('mydata4vipday2.dat'))
-# print(IPX.find('8.8.8.8'))
+IPX.load(os.path.abspath('mydata4vipday2.dat'))
+print(IPX.find('8.8.8.8'))
 
-# def main():
-#     pass
-#
-#
-# if __name__ == '__main__':
-#     main()
