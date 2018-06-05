724ffa65875431adb066fbbd78f336e025f27d42
lg_online <lg_online@hotmail.com>
2018-06-05 20:49:10 +0800

    update 20180605

--- src/practices/ipinfo/checkip.py
+++ src/practices/ipinfo/checkip.py
@@ -17,18 +17,24 @@ def ISIP(s):
 
 def searchFromIP(ip):
     uip = urllib.urlopen('http://m.ip138.com/ip.asp?ip=%s' % ip)
-    fip = uip.read()
+    fip = uip.read().decode('utf-8')
+    print(fip)
     try:
         if fip != "":
-            rip = re.compile(r"您查询的IP：(\d+.\d+.\d+.\d+).*本站主数据：(\S+  \S+)</p>.*参考数据一：(\S+ \S+)</p>.*网友提交的IP：(\S+ \S+)</p>")
+            rip = re.compile(r"您查询的IP：(\d+.\d+.\d+.\d+).*本站主数据：(\S+)</p>.*参考数据一：(\S+\s{1,4}\S+)</p>.*网友提交的IP：(\S+ \S+)</p>")
+            # rip = re.compile(r"本站主数据：(\S+\s{1,4}\S+)</p>.*参考数据一：(\S+\s{1,4}\S+)</p>.*网友提交的IP：(\S+ \S+)</p>.*")
             results = rip.findall(fip)
         else:
-            rip = re.compile(r"您查询的IP：(\d+.\d+.\d+.\d+).*本站主数据：(\S+  \S+)</p>.*")
+            rip = re.compile(r"您查询的IP：(\d+.\d+.\d+.\d+).*本站主数据：(\S+\s{1,4}\S+)</p>.*参考数据一：(\S+\s{1,4}\S+)</p>.*")
+            # rip = re.compile(r"本站主数据：(\S+\s{1,4}\S+)</p>.*参考数据一：(\S+\s{1,4}\S+)</p>.*")
             results = rip.findall(fip)
     except Exception as e:
         print(e)
 
-    print('%s\t %s' % (ip,results.decode('utf8')))
+    # print('%s\t %s' % (ip,str(results).decode('utf8')))
+    for result in results:
+        for r in result:
+            print(r.decode('utf8'))
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
---
+++ src/practices/ipinfo/handle_ping.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/5 20:40
+# @Author  : liugang9
+# @Email   : mlcc330@hotmail.com
+# @File    : handle_ping.py
+# @Software: PyCharm
+# @license: Apache Licence
+# @contact: 3323202070@qq.com
+
+import multiprocessing
+import re
+import sys,re
+import datetime
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
